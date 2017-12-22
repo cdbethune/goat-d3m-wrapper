c4ee5305f85155c14b455b4b07abb0975d86ba2e
Ubuntu <azunre@PaulLinuxDev.jaavmej2qrwupdw4djg0r2u5ud.bx.internal.cloudapp.net>
2017-12-22 22:28:05 +0000

    adding __init__.py to top level

---
+++ __init__.py
@@ -0,0 +1,8 @@
+__author__ = 'Distil'
+
+__version__ = '1.0.0'
+
+__all__ = [
+           "goat",
+           "reverse_goat",
+           ]
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='GoatThinClient',
-    version='1.0',
+    version='1.0.0',
     description='A thin client for interacting with geocoding microservice from New Knowledge, i.e., Goat',
     packages=['GoatThinClient'],
     install_requires=["requests","typing"],
