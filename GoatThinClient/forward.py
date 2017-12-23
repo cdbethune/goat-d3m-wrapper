7ac4dfa8f6fcb91ea0ef185ec4d16b22f79a2d43
Ubuntu <azunre@gmail.com>
2017-12-23 00:30:18 +0000

    dropping address:str from forward _init_, quick debugging experiment

--- GoatThinClient/forward.py
+++ GoatThinClient/forward.py
@@ -30,7 +30,7 @@ class goat(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
     # make sure to populate this with JSON annotations later
     metadata = metadata_module.PrimitiveMetadata({})
     
-    def __init__(self, address: str, *, hyperparams: Hyperparams, random_seed: int = 0, docker_containers: typing.Dict[str, str] = None)-> None:
+    def __init__(self, *, hyperparams: Hyperparams, random_seed: int = 0, docker_containers: typing.Dict[str, str] = None)-> None:
         super().__init__(hyperparams=hyperparams, random_seed=random_seed, docker_containers=docker_containers)
                 
         self.address = address
