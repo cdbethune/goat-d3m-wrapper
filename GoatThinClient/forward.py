dd42d5a58a0e20518f41259b3c0062b2ff3f1ac4
root <azunre@gmail.com>
2018-01-03 00:26:03 +0000

    attempting to remove installation metadata

--- GoatThinClient/forward.py
+++ GoatThinClient/forward.py
@@ -48,12 +48,7 @@ class goat(PrimitiveBase[Inputs, Outputs, Params, Hyperparams]):
         # Of course Python packages can also have their own dependencies, but sometimes it is necessary to
         # install a Python package first to be even able to run setup.py of another package. Or you have
         # a dependency which is not on PyPi.
-        'installation': [{
-            'type': metadata_module.PrimitiveInstallationType.PIP,
-            'package_uri': 'git+https://gitlab.com/datadrivendiscovery/tests-data.git@{git_commit}#subdirectory=primitives'.format(
-                git_commit=utils.current_git_commit(os.path.dirname(__file__)),
-            ),
-        }],
+        'installation': [{}],
         # The same path the primitive is registered with entry points in setup.py.
         'python_path': 'd3m.primitives.distil.Goat.forward',
         # Choose these from a controlled vocabulary in the schema. If anything is missing which would
