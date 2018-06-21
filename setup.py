47f3fa70b4644a97b6252a6de60374945730af2b
azunre <azunre@PaulLinuxGPUDev.jaavmej2qrwupdw4djg0r2u5ud.bx.internal.cloudapp.net>
2018-06-21 08:01:30 +0000

    minor preliminary edits

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
-setup(name='GoatThinClient',
+setup(name='Goat',
     version='1.0.0',
-    description='A thin client for interacting with geocoding microservice from New Knowledge, i.e., Goat',
+    description='A geocoding service from New Knowledge',
     packages=['GoatThinClient'],
     install_requires=["requests","typing"],
     entry_points = {
         'd3m.primitives': [
-            'distil.Goat.forward = GoatThinClient:goat',
-            'distil.Goat.reverse = GoatThinClient:reverse_goat'
+            'distil.Goat.forward = Goat:goat',
+            'distil.Goat.reverse = Goat:reverse_goat'
         ],
     },
 )
